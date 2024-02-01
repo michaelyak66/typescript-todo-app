06a45e4810ba9352158f56f36005adc633e73c00
Michael Mamman <michaelyak66@gmail.com>
2024-02-01 22:46:12 +0100

    youtube v

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,79 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid";
 
-import confetti from 'canvas-confetti';
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.querySelector<HTMLFormElement>("#new-task-form");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
+const tasks: Task[] = loadTasks();
+
+tasks.forEach(addListItem);
+
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!input || input.value.trim() === "") return;
+  
+  const task: Task = {
+    id: uuidV4(),
+    title: input.value.trim(),
+    completed: false,
+    createdAt: new Date()
+  };
+
+  tasks.push(task);
+
+  addListItem(task);
+  input.value = ""; // Clear the input field after adding the task
+});
+
+function addListItem(task: Task): void {
+  const item = document.createElement("li");
+
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    console.log(tasks)
+    saveTasks();
+
+  })
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+
+  const editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+  editButton.addEventListener("click", () => {
+    // Implement edit functionality here
+    console.log("Edit task:", task);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => {
+    // Implement delete functionality here
+    console.log("Delete task:", task);
+    item.remove(); // Remove the task item from the list
+  });
+  
+  item.append(label, editButton, deleteButton);
+  list?.append(item);
+  saveTasks();
+}
+
+function  saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  return JSON.parse(localStorage.getItem("tasks") || "[]");
+}
